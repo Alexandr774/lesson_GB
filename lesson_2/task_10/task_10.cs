71e6039a70ba128b5a7775beb79cfdb420fe8fa1
Aleksandr <aleksandrcekalin082@gmail.com>
2022-08-21 12:33:33 +0300

    Выполнил задание ко второму семинару

---
+++ lesson_2/task_10/task_10.cs
@@ -0,0 +1,24 @@
+﻿//    Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1 
+
+Console.WriteLine("Введите трех значное число:");
+
+int input = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"вторым числом является: {num2(input)}");
+
+
+int num2(int arg){
+
+int numTwo = arg / 10 % 10;
+
+return numTwo;
+} 
+
+
+
+
+
+
---
+++ lesson_2/task_13/task_13.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введи число: ");
+
+string num(){
+ int input = Convert.ToInt32(Console.ReadLine());
+ string numText = Convert.ToString(input);
+if (numText.Length > 2){
+  return "третья цифра -> " + numText[2];
+}
+else {
+  return "-> третьей цифры нет";
+}
+
+}
+
+Console.WriteLine(num());
