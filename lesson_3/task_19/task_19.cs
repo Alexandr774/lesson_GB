3b197897d86728414418f913f11a8338387ee048
Aleksandr <aleksandrcekalin082@gmail.com>
2022-08-23 20:28:13 +0300

    Выполнил задания к третьему семинару, списал второе задание(не понял как его сделать, от слова совсем)

---
+++ lesson_3/task_19/task_19.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число: ");
+ int number = Convert.ToInt32(Console.ReadLine());
+
+
+ string result = polydromeCheck(number);
+Console.WriteLine(result);
+
+
+string polydromeCheck(int number)
+{
+      if (number < 100000 && number >= 10000)
+    {
+        char[] nums1 = number.ToString().ToCharArray();
+            Array.Reverse(nums1);
+            String num2 = new String(nums1);
+            int number1 = Convert.ToInt32(num2);
+                Console.WriteLine(number1);
+    if(number1 == number)
+    {
+      return  "да"; 
+    } 
+    else 
+    {
+        return "нет";
+    }
+    }
+      throw new Exception("Число не пятизначное");
+}
+
+
+
+
---
+++ lesson_3/task_23/task_23.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+Console.WriteLine("Ввдеите число: ");
+int EnterNum = Convert.ToInt32(Console.ReadLine());
+
+
+
+string CubeNum(int number)
+ {
+   string result = "";
+    for(int i = 1; i <= number; i++)
+    {  
+        result += $"{Math.Pow(i, 3)} ";
+ }
+   
+   return result;
+ }
+
+ Console.WriteLine(CubeNum(EnterNum));
