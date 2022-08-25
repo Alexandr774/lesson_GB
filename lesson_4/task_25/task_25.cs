5b56a304338b28b291fa81367f8ae5a95b189565
Aleksandr <aleksandrcekalin082@gmail.com>
2022-08-25 23:11:42 +0300

    Выполнено задание к четвертому семинару

---
+++ lesson_4/task_25/task_25.cs
@@ -0,0 +1,24 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+Console.WriteLine("Введите первое число: ");
+int numOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numTwo = Convert.ToInt32(Console.ReadLine());
+
+double NumberDegree(int one, int two){
+    double result = Math.Pow(one, two);
+    return  result;
+}
+
+
+double numResult = NumberDegree(numOne, numTwo);
+Console.WriteLine(numResult);
+
+
+
+
+
+
