bd4dec3baddf9af747f0a0923beb351d286fe9ef
Aleksandr <aleksandrcekalin082@gmail.com>
2022-08-27 15:14:05 +0300

    Выполнено задание к пятому семинару

---
+++ lesson_5/task_36/task_36.cs
@@ -0,0 +1,28 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+int[] array = new int[10];
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 101);
+        Console.Write(array[i] + " ");
+    }
+}
+int FindUnevenIndexNumberSumm(int[] array)
+{
+    int summ = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            summ += array[i];
+        }
+    }
+    return summ;
+}
+PrintArray(array);
+Console.WriteLine();
+int display2 = FindUnevenIndexNumberSumm(array);
+Console.WriteLine(display2);
