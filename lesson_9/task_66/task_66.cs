218b4f3ac12e6e3807aa39569b049f61c0bae052
Aleksandr <aleksandrcekalin082@gmail.com>
2022-09-09 20:25:45 +0300

    выполнил задание к девятому семминару

---
+++ lesson_9/task_66/task_66.cs
@@ -0,0 +1,22 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+Console.Write("Введите значение M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int summ = 0;
+
+int FindSumm(int numberM, int numberN)
+{
+    if (numberM <= numberN)
+    {
+        summ += numberM;
+        numberM++;
+        FindSumm(numberM, numberN);
+    }
+    return summ;
+}
+Console.WriteLine(FindSumm(numberM, numberN));
